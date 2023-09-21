debe8a67c221de283d1826638f4e8f4d67473ae9
Isaac Ntim <unitedcoresystem@gmail.com>
2023-09-20 20:26:25 -0400

    my first code commit

---
+++ Deploy.java
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Welcome to Zelle"
+echo "Zelle your payment gateway"
+echo "Zelle is the gateway of the future"
+echo "Zelle is offering 40% discount"
+echo "Good deals for returning clients"
